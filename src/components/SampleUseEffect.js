5d039295fc2798c8b05de7057e11e0a6224bb32b
subhinkrishna-sk <subhinkrishna.sk@gmail.com>
2024-09-18 17:48:41 +0530

    updated useeffect with js and state variable

--- src/components/SampleUseEffect.js
+++ src/components/SampleUseEffect.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 const SampleUseEffect = () => {
   const [count, setCount] = useState(0);
+  let jsCount = 0
 
 
 
@@ -17,13 +18,23 @@ const SampleUseEffect = () => {
     console.log('Captured count variable updation');
   },[count]);
 
+  const updateVar = () => {
+    console.log("Var before updation: ",jsCount)
+    setCount(count + 1)
+    jsCount=jsCount+1
+    console.log("Var after updation: ",jsCount)
+  }
 
+  console.log("Var in Component: ",jsCount)
 
   return (
     <div style={{width: 'fit-content', margin: '30px auto'}}>
       <h2 className='mt-3'><u>useEffect HOOK</u></h2>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <div className='d-flex justify-content-between'>
+      <p className='m-3 border p-2 rounded'>Count(useState variable): {count}</p>
+      <p className='m-3 border p-2 rounded'>Count(Js variable): {jsCount}</p>
+      </div>
+      <button onClick={() => {updateVar()}}>Increment</button>
       
       <nav className="nav justify-content-end">
           <a className="m-2 p-2 text-primary " role='button' style={{textDecoration: 'none'}} href="/cleanup"><u>Try Clean Up Function</u></a>
